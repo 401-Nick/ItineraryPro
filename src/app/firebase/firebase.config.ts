30005d4cdf5ed56dc926bf691a2b22e2c01b3b71
401-Nick <95726171+sneaky0129@users.noreply.github.com>
2023-09-19 11:18:59 -0500

    before firebase init

---
+++ src/app/firebase/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDe3K0u3VQhLNX7XQhG7SebYXWUrazDvno",
+    authDomain: "securr-chat.firebaseapp.com",
+    projectId: "securr-chat",
+    storageBucket: "securr-chat.appspot.com",
+    messagingSenderId: "7691235563",
+    appId: "1:7691235563:web:9617bfadf84d99c5f40eae",
+    measurementId: "G-7E0YDWDXPH"
+};
