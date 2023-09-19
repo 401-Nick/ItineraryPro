dbff9e9813a57c81070a04fa861a9d4498a7f74c
401-Nick <95726171+sneaky0129@users.noreply.github.com>
2023-09-19 13:20:28 -0500

    setup to init next step

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from './app/firebase/firebase.config'
+import { getAnalytics } from 'firebase/analytics';
 
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
